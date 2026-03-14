038f05ba26b4a7ada24742fd4c3f1f59064e4179
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-14 23:30:35 +0600

    components crate nav , footer added

---
+++ src/Components/Login/LogIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogIn = () => {
+    return (
+        <div>
+            <h1>Log In sing in sing out log out sin up menans no accout </h1>
+        </div>
+    );
+};
+
+export default LogIn;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../Root/Root";
+import About from "../Components/About/About";
 
 const router = createBrowserRouter([
     {
         path:'/',
-        Component:Root
+        Component:Root,
+        children:[
+            {index:true,Component:About},
+            {}
+        ]
     }
 ])
 export default router

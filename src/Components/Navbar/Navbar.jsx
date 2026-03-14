3f3ec88b46fd719485ed7b8a66268cbab892f67d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-14 23:33:09 +0600

    Nested routes setup

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Navbar = () => {
     return (
         <div>
-            <h1>Navbar</h1>
+           <Link>Home</Link>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,14 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../Root/Root";
 import About from "../Components/About/About";
+import LogIn from "../Components/Login/LogIn";
+import Home from "../Components/Home/Home";
 
 const router = createBrowserRouter([
     {
         path:'/',
         Component:Root,
         children:[
-            {index:true,Component:About},
-            {}
+            {index:true,Component:Home},
+            {path:'about',Component:About},
+            {path:'login',Component:LogIn}
         ]
     }
 ])

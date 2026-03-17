fdf0fc96266c184d5f280fa175748269d9b384cd
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-17 10:17:28 +0600

    google auth

--- src/Components/Login/LogIn.jsx
+++ src/Components/Login/LogIn.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const LogIn = () => {
     return (
         <div>
-            <h1>Log In sing in sing out log out sin up menans no accout </h1>
+            <h1>Log In </h1>
+            <button>Sing Up With Google</button>
         </div>
     );
 };

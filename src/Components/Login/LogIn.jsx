0ad990fd2f2b7c49283fd5b00604e13f680f09fa
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-17 11:46:05 +0600

    github method

--- src/Components/Login/LogIn.jsx
+++ src/Components/Login/LogIn.jsx
@@ -1,56 +1,72 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
 // import auth;
 import { auth } from '../../firebase/firebase.init';
 // import provider;
 const googleProvider = new GoogleAuthProvider()
 
-// useState 
+// github prvider;
+const githubProvider = new GithubAuthProvider();
 
 
 const LogIn = () => {
-    const [users,setUsers] = useState(null);
-    const handleGoogle = ()=>{
-        signInWithPopup(auth,googleProvider)
-        .then(result=>{
-            setUsers(result.user)
-            console.log(result.user);
+    const [users, setUsers] = useState(null);
+    const handleGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                setUsers(result.user)
+                console.log(result.user);
 
-        }).catch(error=>{
-            console.log(error);
-        })
+            }).catch(error => {
+                console.log(error);
+            })
         console.log('google btn clicked');
     }
 
 
-// handle singOut;
-const handleSingOut = ()=>{
-    console.log('sing out btn clicked');
-    signOut(auth)
-    .then(()=>{
-        console.log('successfully sing out')
-          setUsers(null)
-    })
-    .catch(error=>{
-        console.log(error);
-    })
-  
-}
+    // handle github;
+    const handleGithub = () => {
+        console.log('clikde gutbt bnt');
+        signInWithPopup(auth, githubProvider)
+            .then(res => {
+                setUsers(res.user)
+            }).catch(error => {
+                console.log(error);
+            })
+    }
+
+
+    // handle singOut;
+    const handleSingOut = () => {
+        console.log('sing out btn clicked');
+        signOut(auth)
+            .then(() => {
+                console.log('successfully sing out')
+                setUsers(null)
+            })
+            .catch(error => {
+                console.log(error);
+            })
+
+    }
     return (
         <div>
             <h1>Log In </h1>
-           
-          {users &&
-              <div>
-                <h1>name:{users?.displayName}</h1>
-                <h1>email:{users.email}</h1>
-                <img src={users.photoURL} alt="" />
-            </div>
-          }
-          {
-            users?  <button onClick={handleSingOut}>sing Out</button>: <button onClick={handleGoogle}>Sing Up With Google</button>
-          }
-           
+
+            {users &&
+                <div>
+                    <h1>name:{users?.displayName}</h1>
+                    <h1>email:{users.email}</h1>
+                    <img src={users.photoURL} alt="" />
+                </div>
+            }
+            {
+                users ? <button onClick={handleSingOut}>sing Out</button> : <>
+                    <button onClick={handleGithub}>Sing Up With Github</button>
+                    <button onClick={handleGoogle}>Sing Up With Google</button>
+                </>
+            }
+
         </div>
     );
 };

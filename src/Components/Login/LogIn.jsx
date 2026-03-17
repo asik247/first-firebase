b15956a5d667d7836b17bf0528b51002a14c0f8f
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-17 10:20:30 +0600

    popup show

--- src/Components/Login/LogIn.jsx
+++ src/Components/Login/LogIn.jsx
@@ -1,10 +1,23 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../../firebase/firebase.init';
+
+const googleProvider = GoogleAuthProvider()
 
 const LogIn = () => {
+    const handleGoogle = ()=>{
+        signInWithPopup(auth,googleProvider)
+        .then(result=>{
+            console.log(result);
+        }).catch(error=>{
+            console.log(error);
+        })
+        console.log('google btn clicked');
+    }
     return (
         <div>
             <h1>Log In </h1>
-            <button>Sing Up With Google</button>
+            <button onClick={handleGoogle}>Sing Up With Google</button>
         </div>
     );
 };
